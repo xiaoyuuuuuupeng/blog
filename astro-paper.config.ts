29f5ad47eaf959e642e3c10344ca1c2e37cd379b
Xiaoyu Peng <pxyc@gmail.com>
2026-09-08 09:23:49 +0800

    Use pengxiaoyu.info as site URL

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,7 +2,7 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://blog-eta-bay-41.vercel.app/",
+    url: "https://pengxiaoyu.info/",
     title: "xiaoyuuuuuupeng",
     description: "Java、Agent 工程实践与代码审查记录。",
     author: "xiaoyuuuuuupeng",
